5a90d029105d2faec477d9c16957393415d32b3d
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2024-01-06 22:23:56 +0100

    Adding SearchBox Component to page app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import SearchBox from './components/SearchBox'
 
 export default function Home() {
   return (
     <div>
-      hello
+      <SearchBox />
     </div>
   )
 }
